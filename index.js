1732975780232eff8d5ca7290f225a17424a993e
Marciana Davis <davismarciana1@gmail.com>
2018-01-13 03:27:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,5 +21,7 @@ function sayHiToGrandma(string){
   else if (string.toUpperCase() === string){
     return "YES INDEED!"
   }
-  
+  else if (string.toUpperCase() === string){
+    return "I love you, too."
+  }
 }
